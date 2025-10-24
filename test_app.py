92ad570ef193f8e85ee5cb5e76cecdb2a7dc65f8
ESET0082 <gopalbhai.bhammar@esyasoft.com>
2025-10-24 12:59:44 +0530

    add yml file in task2

---
+++ test_app.py
@@ -0,0 +1,19 @@
+
+# def add(a, b):
+#     return a + b
+
+# def subtract(a, b):
+#     return a - b
+
+
+# # ✅ Each test function must start with 'test_'
+# def test_add():
+#     assert add(2, 3) == 5
+
+# # def test_subtract():
+# #     assert subtract(10, 4) == 6
+
+# # def test_add_negative():
+# #     assert add(-1, 1) == 0
+def test_example():
+    assert 2 == 2
